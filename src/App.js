8235bc703e0227a686c23515876dd98824681da8
Emerson Carvalho <brecarv@outlook.com>
2020-10-02 23:56:17 -0300

    App att

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-          // JSX to render goes here...
-        );
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Don Almar</h5>
+          <h6 class="card-subtitle mb-2 text-muted">donal@gmail.com</h6>
+          <p class="card-text">La plata!</p>
+        </div>
+      </div>
+    );
   }
 }
 
